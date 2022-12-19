e150bb74f8a55c8fda2cda0a30c065eb15fdab8e
ivanknz <ehsanur119802@gmail.com>
2022-12-20 01:03:26 +0600

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:941085776816:web:fb69e99823c8fff9f3d475";
+  static String apiKey = "AIzaSyAqoLK3_w11nvvuFGLegtETH0vxYSmuUeI";
+  static String messagingSenderId = "941085776816";
+  static String projectId = "uustudygroup";
+  final primaryColor = const Color(0xFF349ce4);
+}
